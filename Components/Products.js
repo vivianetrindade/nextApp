e3877fc1cc75ac19e2cc083d3288d6e0e970fa27
Salt Dev <saltdev@Salts-MacBook-Pro-3.local>
2022-07-11 19:40:54 +0200

    added images

--- Components/Products.js
+++ Components/Products.js
@@ -1,9 +1,9 @@
 function Products({ productName1, productDescription, productPrice, productImage}) {
-     // let { file, description } = productImage;
+      let { file, description } = productImage;
     return (
         <div className="product">
             <h1>{productName1}</h1>
-            <img src={productImage} />
+            <img src={`https:${file.url}`} alt={description} />
             <p>{productDescription}</p>
             <p>{productPrice}</p>
         </div>
