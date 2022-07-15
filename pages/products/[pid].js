42c9df7bd11ee6b3f03c73bb27463cb513477901
Salt Dev <saltdev@Salts-MacBook-Pro-3.local>
2022-07-15 09:08:29 +0200

    testing

--- pages/products/[pid].js
+++ pages/products/[pid].js
@@ -18,7 +18,7 @@ export const getStaticPaths = async () => {
 }
 export async function getStaticProps(context) {
     const { pid } = context.params;
-    const res = await fetch(`http://localhost:3000/api/product/${pid}`, {method: 'GET'});
+    const res = await fetch(`https://zippy-dolphin-faad4d.netlify.app/api/product/${pid}`);
     const product = await res.json();
 
     const res2 = await fetchEntry(pid);
